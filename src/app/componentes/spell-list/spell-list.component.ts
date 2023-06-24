6dce3974c6cb6a513319b965ba3b4ba0ca8b9b19
JosephV95 <viltejl@gmail.com>
2023-06-24 15:24:52 -0300

    Se desarrolla el componente de lista de hechizos

--- src/app/componentes/spell-list/spell-list.component.ts
+++ src/app/componentes/spell-list/spell-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SpellsAPIService } from 'src/app/servicios/spells-api.service';
 
 @Component({
   selector: 'app-spell-list',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./spell-list.component.scss']
 })
 export class SpellListComponent {
+
+  Hechizos:any = []
+
+  constructor(private spellsApi: SpellsAPIService){}
+
+  ngOnInit():void{
+    this.spellsApi.obtenerSpells().subscribe(data =>{
+      this.Hechizos = data;
+    })
+  }
 
 }
