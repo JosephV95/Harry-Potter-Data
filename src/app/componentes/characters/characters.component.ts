a4a0d76f31a15c960c54946c4d4c112b2924a00a
JosephV95 <viltejl@gmail.com>
2023-07-10 14:37:49 -0300

    Se arreglo el funcionamiento de los filtros y se mejoro el tamaño de los switchs

--- src/app/componentes/characters/characters.component.ts
+++ src/app/componentes/characters/characters.component.ts
@@ -35,36 +35,43 @@ export class CharactersComponent {
     // console.log(this.categorias);
     this.p = 1  //* Esto hara que seleccionar o no un categoria vuelva a la pagination 1
 
-    if (this.categorias.length > 0) {
-      if (this.categorias.includes("otras") ) {
-        this.Personajes = this.filtroSearch().filter(pers => this.categorias.includes(pers.house) || (!pers.house && pers.wizard))
-      }
-       else{
-        this.Personajes = this.filtroSearch().filter(pers => this.categorias.includes(pers.house ) )
-      }
-    } else {
-      this.Personajes = this.filtroSearch();
-    }
-    // console.log(this.Personajes);
+    // ! ahora el filtro por search se podra usar al checkear una categoria
+    this.filtroSearch()
   }
 
-  //*  el filtro por search se guardo en una función para que pueda ser usado para otro tipo de categorias a futuro, por eso no guardo como una variable dentro de categActiva(),   
+  //*  el filtro por search se guardo en una función para que pueda ser usado para otro tipo de categorias a futuro, por eso no guardo como una variable dentro de categActiva()
   filtroSearch(){
     let primerFiltro = this.BackupPersonajes.filter(ev =>
       ev.name.toLowerCase().includes(this.inputSearch.toLowerCase())
     )
     // console.log(primerFiltro);
     
-    return this.Personajes = primerFiltro
+    //! Movi este condicional desde el categActiva()
+    if (this.categorias.length > 0) {
+      if (this.categorias.includes("otras") ) {
+        this.Personajes = primerFiltro.filter(pers => this.categorias.includes(pers.house) || (!pers.house && pers.wizard))
+      }
+       else{
+        this.Personajes = primerFiltro.filter(pers => this.categorias.includes(pers.house ) )
+      }
+    } else {
+      this.Personajes = primerFiltro;
+    }
+    // return this.Personajes
+    
+    return this.Personajes
+    // return this.Personajes = primerFiltro
   }
 
   catMuggles(){
     this.categorias = [] //? Con esto se desmarcaran los checkbox de las casas
+    this.inputSearch = "" //? Hara que el input search este vacio
     this.Personajes = this.BackupPersonajes.filter(per => !per.wizard && per.species == 'human')
   }
 
   catOtrasEsp(){
     this.categorias = [] //? Con esto se desmarcaran los checkbox de las casas
+    this.inputSearch = "" //? Hara que el input search este vacio
     this.Personajes = this.BackupPersonajes.filter(per => per.species != 'human')
   }
 }
